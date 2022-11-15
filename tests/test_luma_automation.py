49f573367a06155e91ee635fc158952db199fcf6
ivanovajulika <julika77@mail.ru>
2022-11-15 16:47:23 +0700

    new test

--- tests/test_luma_automation.py
+++ tests/test_luma_automation.py
@@ -10,3 +10,9 @@ def test_go_to_tees_page(driver):
     menu_women = driver.find_element(By.LINK_TEXT, "Women")
     menu_women.click()
     assert "women" in driver.current_url, "Wrong page"
+
+
+def test_go_men_page(driver):
+    menu_men = driver.find_element(By.LINK_TEXT, "Men")
+    menu_men.click()
+    assert "men" in driver.current_url, "Wrong page"
