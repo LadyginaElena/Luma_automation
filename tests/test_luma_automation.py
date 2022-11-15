57a64e9d9c07188c38c572adc452d4986d20e4ab
ivanovajulika <julika77@mail.ru>
2022-11-15 14:01:43 +0700

    test commit

--- tests/test_luma_automation.py
+++ tests/test_luma_automation.py
@@ -18,3 +18,6 @@ def test_go_to_tees_psge_too(driver):
     menu_women = driver.find_element(By.LINK_TEXT, "Women")
     menu_women.click()
     assert "tees" in driver.current_url, "Wrong page error"
+
+
+# test comment
