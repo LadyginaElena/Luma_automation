5927a30c72dccd25a67e5f4d648341457ea7f481
Elena Ladygina <Ladygina_ev@yahoo.com>
2022-11-15 11:10:40 +0700

    xfail test

--- tests/test_luma_automation.py
+++ tests/test_luma_automation.py
@@ -1,3 +1,4 @@
+import pytest
 from selenium.webdriver.common.by import By
 
 # Создать автоматический тест по следующему сценарию:
@@ -10,3 +11,10 @@ def test_go_to_tees_page(driver):
     menu_women = driver.find_element(By.LINK_TEXT, "Women")
     menu_women.click()
     assert "women" in driver.current_url, "Wrong page"
+
+
+@pytest.mark.xfail
+def test_go_to_tees_psge_too(driver):
+    menu_women = driver.find_element(By.LINK_TEXT, "Women")
+    menu_women.click()
+    assert "tees" in driver.current_url, "Wrong page"
