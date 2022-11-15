c5d4c2600a44d5db257b3b10e001e2efee792c03
Elena Ladygina <Ladygina_ev@yahoo.com>
2022-11-15 14:17:44 +0700

    black and flake8

--- tests/test_luma_automation.py
+++ tests/test_luma_automation.py
@@ -1,4 +1,3 @@
-import pytest
 from selenium.webdriver.common.by import By
 
 # Создать автоматический тест по следующему сценарию:
@@ -11,6 +10,3 @@ def test_go_to_tees_page(driver):
     menu_women = driver.find_element(By.LINK_TEXT, "Women")
     menu_women.click()
     assert "women" in driver.current_url, "Wrong page"
-
-
-
